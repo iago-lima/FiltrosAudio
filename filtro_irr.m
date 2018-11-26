ea590066738e79283275b63e9b12d57ac983e38b
iago.oliveira <iago.oliveira@alu.ufc.br>
2018-11-26 17:04:30 -0300

    Criação dos gráficos e inserção da funcionalidade de reprodução do áudio filtrado

--- filtro_irr.m
+++ filtro_irr.m
@@ -8,29 +8,9 @@ f= [0:N]*(fs/N); % tamanho do grafico de 0 a frequencia da amostragem
 
 [num, den] = butter(20, 0.11);
 [H,W] = freqz(num, den, 512, fs);
-figure(1);
-plot(W, abs(H), 'color', 'blue');
-title('ESPECTRO DO AUDIO ORIGINAL NA FREQUENCIA COM FILTRO BUTTERWORTH');
-xlabel('FREQUENCIA (Hz)');
-ylabel('MAGNITUDE');
-
-grid on;
-hold on;
 
 %Espectro do sinal filtrado
-p = filter(num, den, x); %filtra o sinal
+p = filter(num, den, x); 
 P = fft(p);
-figure(2);
-plot(f(1:N/2), abs(P(1:N/2)));
-title('ESPECTRO DE FREQUENCIA DO SINAL FILTRADO');
-xlabel('FREQUENCIA (Hz)');
-ylabel('MAGNITUDE');
-grid on;
-
-figure(3);
-plot(p,'color','blue');
-title('GRAFICO DO AUDIO FILTRADO NO TEMPO');
-xlabel('Tempo');
-ylabel('Amplitude');
-
-wavwrite(p, fs, 'audio_iir.wav');
+soundsc(p, fs);
+  
--- teste.m
+++ teste.m
@@ -9,21 +9,19 @@ f= [0:N]*(fs/N); % tamanho do grafico de 0 a frequencia da amostragem
 %Recorte do audio de 1 a 8 segundos
 N = length(x);
 t = (0 : N - 1)/fs;
-
 y_sub = x((t >= 1) & (t <= 7));
+
 N = length(y_sub) - 1;
 f = 0:fs/N:fs;
-
 X = abs(fft(y_sub));
 
 %Espectro do filtro FIR
-
-num = fir1(20,[0.11]); 
-[H,w] = freqz(num, 1, 512, fs); 
-
-%Passa o filtro para o dominio da frequencia
+num = fir1(447,[0.11]); %Passa banda com janela de Hamming de ordem 447
+[H,w] = freqz(num, 1, 512, fs); %Passa o filtro para o dominio da frequencia
 
 y_teste = conv(w, y_sub);
+
+
 
 soundsc(y_teste, fs);
 
